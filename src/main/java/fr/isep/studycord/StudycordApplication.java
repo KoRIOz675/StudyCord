1f08a1fe8e0748ec525af6108e74a65d201fdc5d
KoRIOz675 <thomas@mafille.net>
2026-04-28 18:58:17 +0200

    MINOR: build: Added initial build for Spring

---
+++ src/main/java/fr/isep/studycord/StudycordApplication.java
@@ -0,0 +1,13 @@
+package fr.isep.studycord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudycordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudycordApplication.class, args);
+	}
+
+}
